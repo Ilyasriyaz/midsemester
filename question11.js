4202c5b8b53e07f3f6daf87ec2945f0ff5cfef3e
Ilyas Mwale <bis22-imwale@mubas.ac.mw>
2025-08-06 10:12:05 +0100

    last commit

---
+++ question11.js
@@ -0,0 +1,9 @@
+// literal object
+const orderLiteral = {
+    orderID: "ORD-2025-00456";
+    customer: {
+        id: "CUST-1001";
+        name: "Joe Doe";
+        email: "joe.doe@gmail.com";
+    }
+}
